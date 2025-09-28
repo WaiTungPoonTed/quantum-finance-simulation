5cd89d7557464ad0b4cca9073c7bf63daed49ec8
Ted Poon <waitungpoon.maths@gmail.com>
2025-09-29 02:29:53 +0800

    feat: adding optimisation api call handler

---
+++ src/deployments/handler/app.py
@@ -0,0 +1,73 @@
+from adapters import YFinanceFetchStockData
+from entities import Assets, Portfolio
+from adapters import QuboTwoAssetsOptimiser
+import pandas as pd
+import numpy as np
+
+
+def handle_optimise_portfolio(payload: Assets) -> dict:
+    """
+    Accepts a list of assets and optimisation parameters,
+    and will eventually return the optimised portfolio weights.
+    """
+    # FastAPI automatically validates the incoming request body against the Assets model.
+    # If the data is invalid, FastAPI will automatically return a 422 Unprocessable Entity error.
+
+    print("--- FastAPI Service ---")
+    print(f"Received valid assets payload: {payload}", type(payload))
+
+    # --- Fetch Data ---
+    assets = YFinanceFetchStockData(payload)
+    stock_data = assets.fetch()
+    print(f"Fetched stock data: {stock_data}")
+    
+    
+    qubo_two_assets_optimiser = QuboTwoAssetsOptimiser(
+        data=stock_data,
+        risk_free_rate=0,
+        grover_iteration=3,
+        mu=1,
+        value_qubits=13,
+        n_qubits=4
+    )
+    
+    # --- Optimise Portfolio ---
+    optimised_portfolio = qubo_two_assets_optimiser.optimise()
+    print(f"Optimised portfolio: {optimised_portfolio}")
+
+    
+    
+    
+    # --- MOCK LOGIC ---
+    # In the future, this is where you will call your use_cases and adapters.
+    # For now, we return a hardcoded JSON response.
+    # mock_response = {
+    #     "message": "Data processed successfully by the REAL FastAPI service",
+    #     "input_parameters": payload,
+    #     "optimised_weights": {
+    #         "assets.tickers[0]": 0.55,  # Use different numbers to prove it's from Python
+    #         "assets.tickers[1]": 0.45,
+    #     },
+    #     "expected_return": 0.28,
+    #     "expected_risk": 0.15,
+    # }
+    weights: list[np.float] = [stock[0] for stock in optimised_portfolio]
+    
+    portfolio = stock_data.pct_change() * np.array(weights)
+    # first day fill NaN with 0
+    portfolio = portfolio.fillna(0).sum(axis=1)
+    
+    ret = stock_data.pct_change().fillna(0)
+    ret['Quantum_Portfolio'] = portfolio  
+    # Convert into price
+    df = (1 + ret).cumprod()      
+    mock_response = {
+        'portfolio': Portfolio(
+            tickers=payload.tickers,
+            weights=weights # Use different numbers to prove it's from Python
+        ),
+        'stock_data': df.to_dict()
+    }
+    # print(stock_data.to_dict())
+    print("Sending mock response from FastAPI.")
+    return mock_response
